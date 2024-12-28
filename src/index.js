3f5bb8c527771a8cbb994ebae0a687a68a1c3bbf
Ph-Aitch <aitch.samir44@gmail.com>
2024-12-28 18:34:11 +0200

    main module JS file that runs the page

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./styles.css"
+import { home } from "./home.js";
+import { menu } from "./menu.js";
+import { about } from "./about.js";
+import { signup , form } from "./signup.js";
+
+home
+menu()
+about()
+signup()
+form()
